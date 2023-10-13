749f5b6ae0b1e705d24183eb57b0c6f85c6d2c31
Harivatsa <gaharivatsa@gmail.com>
2023-10-13 23:48:54 +0530

    'hsmll1'

--- src/context/productscontext.js
+++ src/context/productscontext.js
@@ -6,7 +6,7 @@ export const ProductContextProvider = ({ children }) => {
     const [PRODUCTS, setProducts] = useState([]);
 
     useEffect(() => {
-        fetch("None")
+        fetch("https://hsm-indol.vercel.app/api")
             .then((response) => response.json())
             .then((data) => {
                 setProducts(data);
